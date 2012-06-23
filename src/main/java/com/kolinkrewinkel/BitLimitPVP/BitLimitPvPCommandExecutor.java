f7994571d3abcec07db209c81892283dd260ea2b
kolinkrewinkel <kolin@kolinkrewinkel.com>
2012-06-23 04:13:50 -0700

    further customize

---
+++ src/main/java/com/kolinkrewinkel/BitLimitPVP/BitLimitPvPCommandExecutor.java
@@ -0,0 +1,31 @@
+package com.kolinkrewinkel.BitLimitPvP;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+
+import com.google.common.base.Joiner;
+
+public class BitLimitPvPCommandExecutor implements CommandExecutor {
+    private final BitLimitPvP plugin;
+
+    /*
+     * This command executor needs to know about its plugin from which it came from
+     */
+    public SampleCommandExecutor(BitLimitPvP plugin) {
+        this.plugin = plugin;
+    }
+
+    /*
+     * On command set the sample message
+     */
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission("BitLimitPvP.message") && args.length > 0) {
+            this.plugin.getConfig().set("BitLimitPvP.message", Joiner.on(' ').join(args));
+            return true;
+        } else {
+            return false;
+        }
+    }
+
+}
