77be6aca332c33d5889b531c26f7f0e309012d8a
kolinkrewinkel <kolin@kolinkrewinkel.com>
2012-06-24 13:25:11 -0700

    Add XP level incremeting.

--- src/main/java/com/kolinkrewinkel/BitLimitPVP/BitLimitPvPListener.java
+++ src/main/java/com/kolinkrewinkel/BitLimitPVP/BitLimitPvPListener.java
@@ -29,14 +29,14 @@ import static com.sk89q.worldguard.bukkit.BukkitUtil.*;
 public class BitLimitPvPListener implements Listener {
     private final BitLimitPvP plugin; // Reference main plugin
 	private Random rand = new Random(System.nanoTime()); // Random respawn controller
-
+    
     public BitLimitPvPListener(BitLimitPvP plugin) {
         // Register the listener
         plugin.getServer().getPluginManager().registerEvents(this, plugin);
         
         this.plugin = plugin;
     }
-
+    
     /*
      * Send the sample message to all players that join
      */
@@ -44,11 +44,11 @@ public class BitLimitPvPListener implements Listener {
     public void onPlayerJoin(PlayerJoinEvent event) {
         Player player = event.getPlayer(); // The player who joined
         PlayerInventory inventory = player.getInventory(); // The player's inventory
-
+        
         ItemStack sword = new ItemStack(Material.IRON_SWORD, 1);
         ItemStack bow = new ItemStack(Material.BOW, 1);
         bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
-
+        
         if (!inventory.contains(sword) && !inventory.contains(bow)) {
             ItemStack arrow = new ItemStack(Material.ARROW, 1);
             ItemStack food = new ItemStack(Material.COOKED_BEEF, 8);
@@ -66,53 +66,84 @@ public class BitLimitPvPListener implements Listener {
             inventory.addItem(food);
         }
     }
-
+    
     @EventHandler
     public void onPlayerRespawn(PlayerRespawnEvent event) {
         Player player = event.getPlayer(); // Get player who respawned
         Location respawnLocation = event.getRespawnLocation();
-
-//      Respawn Location Randomization
+        
+        //      Respawn Location Randomization
         WorldGuardPlugin worldGuard = getWorldGuard(player);
         Vector pt = toVector(respawnLocation); // This also takes a location
         
         RegionManager regionManager = worldGuard.getRegionManager(player.getWorld());
         ApplicableRegionSet set = regionManager.getApplicableRegions(pt);
-
+        
         Iterator itr = set.iterator();
         ProtectedRegion region = null;
         while (itr.hasNext()) {
             region = (ProtectedRegion) itr.next();
         }
         event.setRespawnLocation(getRandomLocationInRegionWithPlayerVerifyIntersection(region, player));
-
-//      Auto equipping
+        
+        //      Auto equipping
         PlayerInventory inventory = player.getInventory(); // The player's inventory
-
+        
         ItemStack sword = new ItemStack(Material.IRON_SWORD, 1);
         ItemStack bow = new ItemStack(Material.BOW, 1);
         bow.addEnchantment(Enchantment.ARROW_INFINITE, 1);
         ItemStack arrow = new ItemStack(Material.ARROW, 1);
         ItemStack food = new ItemStack(Material.COOKED_BEEF, 8);
-
+        
         ItemStack[] armor = new ItemStack[4];
         armor[0] = new ItemStack(Material.LEATHER_BOOTS, 1);
         armor[1] = new ItemStack(Material.LEATHER_LEGGINGS, 1);
         armor[2] = new ItemStack(Material.LEATHER_CHESTPLATE, 1);
         armor[3] = new ItemStack(Material.LEATHER_HELMET, 1);
         inventory.setArmorContents(armor);
-
+        
         inventory.addItem(sword);
         inventory.addItem(bow);
         inventory.addItem(arrow);
         inventory.addItem(food);
     }
-
+    
     @EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         event.getDrops().clear(); // Remove drops.
+        
+        // Death causer
+        Player player = (Player)e.getEntity();
+        
+        // Get last damage infliction
+        EntityDamageEvent attacker = player.getLastDamageCause();
+        
+        // Determine if isPlayer
+        if(attacker.getEntity() instanceof Player)
+        {
+            // Get offensive player
+            Player attackerPlayer = (Player)attacker.getEntity();
+            // Increment level by one.
+            attackerPlayer.setLevel(attackerPlayer.getLevel() + 1);
+        }
     }
-
+    
+    @EventHandler
+    public void onEntityDamage(EntityDamageEvent event){
+        if(event.isCancelled() || !(event.getEntity().isDead()))
+            return;
+        
+        Player player = null;
+        if(event.getEntity instanceof Player){
+            player = (Player)event.getEntity();
+        }
+        
+        if(event.getCause().equals(DamageCause.LAVA) && p.isDead()){
+            //code you want
+        }
+    }
+    
+    
     private WorldGuardPlugin getWorldGuard(Player player)
     {
         Plugin plugin = player.getServer().getPluginManager().getPlugin("WorldGuard");
@@ -122,35 +153,35 @@ public class BitLimitPvPListener implements Listener {
         
         return (WorldGuardPlugin)plugin;
     }
-
+    
     private Location getRandomLocationInRegionWithPlayerVerifyIntersection(ProtectedRegion region, Player player)
     {
         Location randomLocation = getRandomLocationInRegionWithPlayer(region, player);
         while (!region.contains(toVector(randomLocation))) {
             randomLocation = getRandomLocationInRegionWithPlayer(region, player);
         }
-
+        
         return randomLocation;
     }
-
+    
     private Location getRandomLocationInRegionWithPlayer(ProtectedRegion region, Player player)
     {
         Vector minPoint = region.getMinimumPoint();
         Vector maxPoint = region.getMaximumPoint();
-
+        
         int diffX = Math.abs((int)maxPoint.getX() - (int)minPoint.getX() + 1);
 		int diffZ = Math.abs((int)maxPoint.getZ() - (int)minPoint.getZ() + 1);
-
+        
         int x = rand.nextInt(diffX) + (int)minPoint.getX();
 		int z = rand.nextInt(diffZ) + (int)minPoint.getZ();
-
-
+        
+        
         // Adapted from RobertZenz/SpawnRandomizer
         int y = player.getWorld().getHighestBlockYAt(x, z);
-
+        
         Location randomLocation = new Location(player.getWorld(), (double)x, (double)y, (double)z);
         
         return randomLocation;
     }
-
+    
 }
