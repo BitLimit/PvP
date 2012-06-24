f311ab0a761c6dcba3ea65758422ed9a27992050
kolinkrewinkel <kolin@kolinkrewinkel.com>
2012-06-24 00:19:28 -0700

    Add info command, uses WorldGuard info.

---
+++ src/main/java/com/kolinkrewinkel/BitLimitPVP/MapCommandExecutor.java
@@ -0,0 +1,72 @@
+package com.kolinkrewinkel.BitLimitPvP;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+import org.bukkit.World;
+
+import com.google.common.base.Joiner;
+import org.bukkit.plugin.Plugin;
+import com.sk89q.worldedit.Vector;
+
+import com.sk89q.worldguard.bukkit.WorldGuardPlugin;
+import com.sk89q.worldguard.LocalPlayer;
+import com.sk89q.worldguard.protection.managers.RegionManager;
+import com.sk89q.worldguard.protection.ApplicableRegionSet;
+import com.sk89q.worldguard.protection.regions.ProtectedRegion;
+import static com.sk89q.worldguard.bukkit.BukkitUtil.*;
+
+public class MapCommandExecutor implements CommandExecutor {
+    private final BitLimitPvP plugin;
+
+    /*
+     * This command executor needs to know about its plugin from which it came from
+     */
+    public MapCommandExecutor(BitLimitPvP plugin) {
+        this.plugin = plugin;
+    }
+
+    /*
+     * On command set the sample message
+     */
+    public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
+        if (sender.hasPermission("BitLimitPvP") && args.length > 0) {
+
+            if (sender instanceof Player) {
+                //Then we cast the sender to player,which means now we can handle him through 'player' variable like any other players
+                Player player = (Player) sender;
+
+                WorldGuardPlugin worldGuard = getWorldGuard(sender);
+                Vector pt = toVector(player.getLocation()); // This also takes a location
+                LocalPlayer localPlayer = worldGuard.wrapPlayer(player);
+                
+                RegionManager regionManager = worldGuard.getRegionManager(player.getWorld());
+                ApplicableRegionSet set = regionManager.getApplicableRegions(pt);
+                while (set.iterator().hasNext()) {
+                    ProtectedRegion region = set.iterator().next();
+                    sender.sendMessage(region.getId());
+                }
+            }
+            
+
+
+//            this.plugin.getConfig().set("BitLimitPvP.message", Joiner.on(' ').join(args));
+            return true;
+        }
+        
+        return false;
+    }
+
+    private WorldGuardPlugin getWorldGuard(CommandSender sender)
+    {
+        Plugin plugin = sender.getServer().getPluginManager().getPlugin("WorldGuard");
+        if ((plugin == null) || (!(plugin instanceof WorldGuardPlugin))) {
+            return null; //throws a NullPointerException, telling the Admin that WG is not loaded.
+        }
+        
+        return (WorldGuardPlugin)plugin;
+    }
+
+
+}
