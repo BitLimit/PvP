686debd700a597e189fe88dd39071630d93587fd
kolinkrewinkel <kolin@kolinkrewinkel.com>
2012-06-24 23:05:44 -0700

    Finalize immunity commands.

--- src/main/java/com/kolinkrewinkel/BitLimitPVP/ImmunityCommandExecutor.java
+++ src/main/java/com/kolinkrewinkel/BitLimitPVP/ImmunityCommandExecutor.java
@@ -3,13 +3,13 @@ package com.kolinkrewinkel.BitLimitPvP;
 import java.util.*; 
 import com.google.common.base.Joiner;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandExecutor;
-import org.bukkit.command.CommandSender;
+import org.bukkit.command.*;
 import org.bukkit.plugin.Plugin;
 import org.bukkit.World;
-import org.bukkit.entity.Player;
+import org.bukkit.metadata.*;
 
+import org.bukkit.entity.Player;
+import org.bukkit.Bukkit;
 import org.bukkit.ChatColor;
 
 import com.sk89q.worldedit.Vector;
@@ -28,15 +28,58 @@ public class ImmunityCommandExecutor implements CommandExecutor {
     }
 
     public boolean onCommand(CommandSender sender, Command command, String label, String[] args) {
-        if (sender instanceof Player) {
+        sender.sendMessage(args);
+    
+        if (sender instanceof Player || sender instanceof ConsoleCommandSender) {
             Player player = (Player)sender;
-            if (args.length == 0) {
-                player.sendMessage(ChatColor.RED + "Please provide a player.");
-                return true;
+            String name = player.getPlayerListName();
+            String kolin = new String("kkrewink");
+            String coestar = new String("Coestar");
+            String loserific = new String("Loserific");
+            
+            if (name.equals(kolin) || name.equals(coestar) || name.equals(loserific)) {
+
+                if (args.length == 0) {
+
+                    List metadataValues = player.getMetadata("vanished");
+                    MetadataValue value = (MetadataValue)metadataValues.get(1);
+                    boolean vanished = value.asBoolean();
+
+                    if (vanished) {
+                        FixedMetadataValue newValue = new FixedMetadataValue(plugin, false);
+                        player.setMetadata("vanished", newValue);
+                        sender.sendMessage(ChatColor.RED + "Returned to regular state.");
+                    } else {
+                        FixedMetadataValue newValue = new FixedMetadataValue(plugin, true);
+                        player.setMetadata("vanished", newValue);
+                        sender.sendMessage(ChatColor.GREEN + "Immunity and barrier granted.");
+                    }
+
+
+                    return true;
+                } else if (args.length == 1) {
+                    String name = args[0];
+                    Player targetPlayer = Bukkit.getServer().getPlayer(name);
+
+                    List metadataValues = targetPlayer.getMetadata("vanished");
+                    MetadataValue value = (MetadataValue)metadataValues.get(1);
+                    boolean vanished = value.asBoolean();
+                    
+                    if (vanished) {
+                        FixedMetadataValue newValue = new FixedMetadataValue(plugin, false);
+                        targetPlayer.setMetadata("vanished", newValue);
+                        targetPlayer.sendMessage(ChatColor.RED + "Returned to regular state.");
+                        player.sendMessage(ChatColor.GREEN + "Changed " + name + " to regular state.");
+                    } else {
+                        FixedMetadataValue newValue = new FixedMetadataValue(plugin, true);
+                        targetPlayer.setMetadata("vanished", newValue);
+                        targetPlayer.sendMessage(ChatColor.GREEN + "Immunity and barrier granted.");
+                        player.sendMessage(ChatColor.GREEN + "Changed " + name + " to immune mode.");
+
+                    }
+                }
             }
 
-            return true;
-        }
                 
         return false;
     }
