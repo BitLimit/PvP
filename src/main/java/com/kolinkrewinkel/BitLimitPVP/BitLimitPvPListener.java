014135f5f0b3b9a2ff208d0563daaee1e82a5264
kolinkrewinkel <kolin@kolinkrewinkel.com>
2012-06-27 03:16:16 -0700

    I ought to stop commiting before compiling.

--- src/main/java/com/kolinkrewinkel/BitLimitPVP/BitLimitPvPListener.java
+++ src/main/java/com/kolinkrewinkel/BitLimitPVP/BitLimitPvPListener.java
@@ -1,11 +1,15 @@
 package com.kolinkrewinkel.BitLimitPvP;
 
 import java.text.MessageFormat;
-import java.util.*; 
+import java.util.*;
 
 import org.bukkit.plugin.Plugin;
 import org.bukkit.entity.*;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
 import org.bukkit.event.player.*;
+import org.bukkit.potion.*;
+
 
 public class BitLimitPvPListener implements Listener {
     private final BitLimitPvP plugin; // Reference main plugin
@@ -19,7 +23,7 @@ public class BitLimitPvPListener implements Listener {
     @EventHandler
     public void onPlayerChangedWorld(PlayerChangedWorldEvent event) {
         // Add a teleportation effect for a couple of seconds
-        PotionEffect confusion = new PotionEffect(PotionEffect.CONFUSION, 150, 1);
+        PotionEffect confusion = new PotionEffect(PotionEffectType.CONFUSION, 150, 1);
         confusion.apply(event.getPlayer());
         
     }
